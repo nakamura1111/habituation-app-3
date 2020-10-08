9a34e04032e985f3d06b336685fcd416d78f5f0d
nakamura1111 <5040.armuakan@gmail.com>
2020-10-08 20:24:21 +0900

    Happiness, Hardness選択バー用のモデル設定

---
+++ app/models/hardness.rb
@@ -0,0 +1,8 @@
+class Hardness < ActiveHash:Base
+  self.data = [
+    {id: 0, name: "未達成"},
+    {id: 1, name: "そんなに大変じゃなかった"},
+    {id: 2, name: "結構しんどかった"},
+    {id: 3, name: "心折れかけた..."}
+  ]
+end
