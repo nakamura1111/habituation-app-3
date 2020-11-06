2c08896f1bb398abcf6aaad092d3b1c834721616
nakamura1111 <5040.armuakan@gmail.com>
2020-11-06 17:22:06 +0900

    jquery導入

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,12 @@
 const { environment } = require('@rails/webpacker')
 
+// jquery導入について：https://qiita.com/masahisa/items/eaacb0c3b82f4a11fc13
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+    new webpack.ProvidePlugin({
+        $: 'jquery/src/jquery',
+        jQuery: 'jquery/src/jquery'
+    })
+)
+
 module.exports = environment
